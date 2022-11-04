1e051f668c2f37b7486931eabcf47d7700168a45
Dmitrii Borisov <rundoom1337@gmail.com>
2022-11-04 17:47:51 +0700

    Clean up

--- zombie/zombie.gd
+++ zombie/zombie.gd
@@ -10,7 +10,6 @@ var current_hp := 4:
 		if value <= 0: queue_free()
 
 func _physics_process(delta: float) -> void:
-#	if !check_hero_collide():
 	current_speed = SPEED
 	var attractor := get_tree().get_first_node_in_group("zombie_attractor") as Node2D
 		
@@ -18,13 +17,5 @@ func _physics_process(delta: float) -> void:
 	rotation = lerp_angle(rotation, target_rotation, 0.1)
 	
 	velocity = Vector2.RIGHT.rotated(rotation) * current_speed
-#	else:
-#		velocity = Vector2.ZERO
 
 	move_and_slide()
-
-
-func check_hero_collide():
-	for i in get_slide_collision_count():
-		if get_slide_collision(i).get_collider().is_in_group("zombie_attractor"):
-			return true
