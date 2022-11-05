2e5730675af6db1e0eb35f6690a6fd0f70b27404
Dmitrii Borisov <rundoom1337@gmail.com>
2022-11-05 17:58:43 +0700

    Refactor

--- hero/hero.gd
+++ hero/hero.gd
@@ -16,6 +16,14 @@ func _physics_process(delta: float) -> void:
 	var mouse_pos := get_global_mouse_position()
 	$RayCast2D.global_rotation = global_position.direction_to(mouse_pos).angle()
 	
+	switch_weapon()
+		
+	$Label.text = str(current_attack_type)
+
+	move_and_slide()
+
+
+func switch_weapon():
 	current_attack_type = AttackType.MELEE if $RayCast2D.is_colliding() else AttackType.RANGED
 	if current_attack_type == AttackType.MELEE:
 		$Ranged.visible = false
@@ -27,7 +35,3 @@ func _physics_process(delta: float) -> void:
 		$Ranged.set_physics_process(true)
 		$Melee.visible = false
 		$Melee.set_physics_process(false)
-		
-	$Label.text = str(current_attack_type)
-
-	move_and_slide()
