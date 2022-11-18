66cd71da5e5fb48c0174655ba0f4e4e7f2f5d5d2
Dmitrii Borisov <rundoom1337@gmail.com>
2022-11-18 18:28:36 +0700

    Pause label added

--- UI/UIContainer.gd
+++ UI/UIContainer.gd
@@ -4,4 +4,4 @@ extends CanvasLayer
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().paused = !get_tree().paused
-	
+		$Paused.visible = get_tree().paused
