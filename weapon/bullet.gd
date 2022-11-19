36c6a193c7574eae5bc36a3a1ae6c2d4e9580eb3
Dmitrii Borisov <rundoom1337@gmail.com>
2022-11-19 22:48:47 +0700

    Bullet impacts

--- weapon/bullet.gd
+++ weapon/bullet.gd
@@ -2,6 +2,8 @@ extends AnimatableBody2D
 
 
 var speed := 1000
+@onready var spare := $SpareBullet as RigidBody2D
+var velocity := Vector2.ZERO
 
 
 func _ready() -> void:
@@ -10,8 +12,19 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	move_and_collide(Vector2.RIGHT.rotated(rotation) * delta * speed)
+	velocity = Vector2.RIGHT.rotated(rotation) * speed
+	move_and_collide(velocity * delta)
 
 
 func _on_lifetime_timeout() -> void:
 	queue_free()
+
+
+func _on_tree_exiting() -> void:
+	var level = get_tree().get_first_node_in_group("level")
+	remove_child(spare)
+	level.add_child(spare)
+	spare.global_transform = global_transform
+	spare.linear_velocity.x = -velocity.x / 2
+	spare.start_acting()
+	
