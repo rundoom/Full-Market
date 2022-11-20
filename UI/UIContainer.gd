a78a9f7f585775d87b9cb93ab0f4f7c337e426e0
Dmitrii Borisov <rundoom1337@gmail.com>
2022-11-20 14:14:31 +0700

    Exit shop on "E"

--- UI/UIContainer.gd
+++ UI/UIContainer.gd
@@ -5,14 +5,14 @@ var is_shopping_avaliable := false
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_cancel") or (event.is_action_pressed("interaction") and is_shopping_avaliable and get_tree().paused):
 		get_tree().paused = !get_tree().paused
 		$Paused.visible = get_tree().paused
 		if get_tree().paused: $ShoppingReminder.visible = false
 		if !get_tree().paused:
 			if is_shopping_avaliable: $ShoppingReminder.visible = true
 			$Shopping.visible = false
-	if event.is_action_pressed("interaction") and is_shopping_avaliable:
+	elif event.is_action_pressed("interaction") and is_shopping_avaliable:
 		get_tree().paused = true
 		$Shopping.visible = true
 		$ShoppingReminder.visible = false
